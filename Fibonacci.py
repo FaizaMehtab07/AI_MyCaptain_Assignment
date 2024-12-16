bcff1e51c295137bdf03d1ec1157121017bf6a78
FaizaMehtab07 <faizamehtab07@gmail.com>
2024-12-16 20:09:24 +0530

    Fibonnaci Number
    Write a Python Program for Fibonacci numbers.

---
+++ Fibonacci.py
@@ -0,0 +1,15 @@
+def fibonacci_iterative(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    
+    fib_sequence = [0, 1]
+    for i in range(2, n):
+        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+    return fib_sequence
+
+n = int(input("Enter the number of Fibonacci terms: "))
+print("Fibonacci sequence (iterative):", fibonacci_iterative(n))
